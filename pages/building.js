6e797e62892388338b20321f9d75435ac4064267
Eric <123097666+HHAhuange@users.noreply.github.com>
2023-06-26 20:08:27 -0400

    Added forms and pie chart functionality - not fully implemented

--- pages/building.js
+++ pages/building.js
@@ -1,20 +1,67 @@
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 15];
-var barColors = ["red", "green","blue","orange","brown"];
+let gasConsumption = 0;
+let elecConsumption = 0;
+let buildingArea = 0;
 
-new Chart("myChart", {
+var xValues = ["NG", "Electricity"];
+var yValues = [55, 45];
+var yValues2 = [65, 35];
+
+var barColors = ["#71eff8", "#b8f7fc"];
+
+new Chart("energy", {
     type: "pie",
     data: {
       labels: xValues,
       datasets: [{
         backgroundColor: barColors,
-        data: yValues
+        data: yValues,
+        borderWidth: 10
       }]
     },
     options: {
       title: {
         display: true,
-        text: "World Wide Wine Production"
+        text: "Total Energy GJ",
+      
+      layout: {
+        padding: 20
+      }
+      
+    }
+  }});
+
+new Chart("electricity", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues2,
+        borderWidth: 10
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Total Energy GJ"
       }
     }
   });
+  
+function storeValue(element) {
+    if (element.id == "gas") { 
+        gasConsumption = element.value;
+        console.log(gasConsumption);
+    }
+
+    else if (element.id == "elec") {
+        elecConsumption = element.value;
+        console.log(elecConsumption);
+    }
+
+    else if (element.id == "area") {
+        buildingArea = element.value;
+        console.log(buildingArea);
+    }
+}
+  
