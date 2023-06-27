bb11ffc56ba65d6fa73000881077f2fec333659a
Eric <123097666+HHAhuange@users.noreply.github.com>
2023-06-27 11:21:20 -0400

    Update pie chart padding and styling

--- pages/building.js
+++ pages/building.js
@@ -5,8 +5,9 @@ let buildingArea = 0;
 var xValues = ["NG", "Electricity"];
 var yValues = [55, 45];
 var yValues2 = [65, 35];
+var yValues3 = [25, 75];
 
-var barColors = ["#71eff8", "#b8f7fc"];
+var barColors = ["#29C3EC", "#DBEFFF"];
 
 new Chart("energy", {
     type: "pie",
@@ -21,16 +22,26 @@ new Chart("energy", {
     options: {
       title: {
         display: true,
-        text: "Total Energy GJ",
-      
-      layout: {
-        padding: 20
-      }
-      
-    }
-  }});
+        text: "Total Energy (GJ)",
+      },
+      aspectRatio: 1
+    },
+    plugins: {
+      datalabels: {
+          formatter: (value, ctx) => {
+              let sum = 0;
+              let dataArr = ctx.chart.data.datasets[0].data;
+              dataArr.map(data => {
+                  sum += data;
+              });
+              let percentage = (value*100 / sum).toFixed(2)+"%";
+              return percentage;
+          }
+        }
+      },
+});
 
-new Chart("electricity", {
+new Chart("emissions", {
     type: "pie",
     data: {
       labels: xValues,
@@ -43,8 +54,28 @@ new Chart("electricity", {
     options: {
       title: {
         display: true,
-        text: "Total Energy GJ"
-      }
+        text: "Total Emissions (TCO2e)",
+      },
+      aspectRatio: 1
+    }
+  });
+
+  new Chart("costs", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues3,
+        borderWidth: 10
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Total Cost ($)",
+      },
+      aspectRatio: 1
     }
   });
   
